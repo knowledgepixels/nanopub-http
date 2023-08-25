2e12b67d75420ee68d144c8382edd800d0685dab
Tobias Kuhn <kuhntobias@gmail.com>
2023-08-25 16:47:34 +0200

    Rudimentary publishing functionality

--- src/main/java/com/knowledgepixels/nanopub/http/MainVerticle.java
+++ src/main/java/com/knowledgepixels/nanopub/http/MainVerticle.java
@@ -1,15 +1,30 @@
 package com.knowledgepixels.nanopub.http;
 
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.Arrays;
 
 import org.apache.http.HttpStatus;
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.rio.RDFFormat;
 import org.nanopub.MalformedNanopubException;
 import org.nanopub.Nanopub;
 import org.nanopub.NanopubImpl;
+import org.nanopub.NanopubUtils;
+import org.nanopub.extra.security.SignNanopub;
+import org.nanopub.extra.security.SignatureAlgorithm;
+import org.nanopub.extra.security.TransformContext;
+import org.nanopub.extra.server.PublishNanopub;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import net.trustyuri.TrustyUriException;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -23,27 +38,39 @@ public class MainVerticle extends AbstractVerticle {
 	public void start(Promise<Void> startPromise) throws Exception {
 		vertx.createHttpServer().requestHandler(req -> {
 			try {
-				final StringBuilder payload = new StringBuilder();
-				req.handler(data -> {
-					payload.append(data.toString("UTF-8"));
-				});
-				req.endHandler(handler -> {
-					final String dataString = payload.toString();
-					try {
-						Nanopub np = new NanopubImpl(dataString, RDFFormat.TRIG);
-						// TODO: sign, transform, and publish
-						System.err.println("LOADED:\n\n" + dataString);
-					} catch (MalformedNanopubException ex) {
-						req.response().setStatusCode(HttpStatus.SC_BAD_REQUEST)
-							.setStatusMessage(Arrays.toString(ex.getStackTrace()))
+				System.err.println(req.uri());
+				if (req.path().matches("/publish")) {
+					final StringBuilder payload = new StringBuilder();
+					req.handler(data -> {
+						payload.append(data.toString("UTF-8"));
+					});
+					req.endHandler(handler -> {
+						final String dataString = payload.toString();
+						try {
+							Nanopub np = new NanopubImpl(dataString, RDFFormat.TRIG);
+							System.err.println("PARAM: " + req.getParam("test"));
+							// TODO: sign, transform, and publish
+							KeyPair keys = SignNanopub.loadKey("~/.nanopub/id_rsa", SignatureAlgorithm.RSA);
+							IRI signer = vf.createIRI(req.getParam("signer"));
+							TransformContext c = new TransformContext(SignatureAlgorithm.RSA, keys, signer, false, false);
+							Nanopub transformedNp = SignNanopub.signAndTransform(np, c);
+							System.err.println("TRANSFORMED:\n\n" + NanopubUtils.writeToString(transformedNp, RDFFormat.TRIG));
+							PublishNanopub.publish(transformedNp);
+							System.err.println("PUBLISHED: " + transformedNp.getUri());
+						} catch (MalformedNanopubException | SignatureException | NoSuchAlgorithmException | InvalidKeySpecException | IOException | InvalidKeyException | TrustyUriException ex) {
+							req.response().setStatusCode(HttpStatus.SC_BAD_REQUEST)
+								.setStatusMessage(Arrays.toString(ex.getStackTrace()))
+								.end();
+							ex.printStackTrace();
+							return;
+						};
+						req.response()
+							.setStatusCode(HttpStatus.SC_OK)
 							.end();
-						ex.printStackTrace();
-						return;
-					};
-					req.response()
-						.setStatusCode(HttpStatus.SC_OK)
-						.end();
-				});
+					});
+				} else {
+					throw new RuntimeException("Unknown request path: " + req.path());
+				}
 			} catch (Exception ex) {
 				req.response().setStatusCode(HttpStatus.SC_BAD_REQUEST)
 					.setStatusMessage(Arrays.toString(ex.getStackTrace()))
@@ -59,5 +86,7 @@ public class MainVerticle extends AbstractVerticle {
 			}
 		});
 	}
+
+	private static final ValueFactory vf = SimpleValueFactory.getInstance();
 
 }
